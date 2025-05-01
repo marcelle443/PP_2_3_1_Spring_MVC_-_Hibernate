49289e34536d3897aa8efa7bfacb6e3e3222ff64
Mars <sharifullin44@yandex.ru>
2025-05-01 22:09:22 +0500

    first_commit_

---
+++ src/main/java/hiber/config/MySPringMVCDispatcherServletInitializer.java
@@ -0,0 +1,23 @@
+package src.main.java.hiber.config;
+
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySPringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
